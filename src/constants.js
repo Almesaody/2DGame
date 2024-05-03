c2229d9eb335a77ed115455c51c4256ce085f1af
Almesaody <120131207+Almesaody@users.noreply.github.com>
2024-05-04 02:08:14 +0300

    started adding dialogue and new props to map

--- src/constants.js
+++ src/constants.js
@@ -1,23 +1,9 @@
 export const scaleFactor = 4;
 
 export const dialogueData = {
-    pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
-        I've made a couple of games in that language. I also like Golang and Python. Anyway regardless of the language, I just like programming.
-        Here is my <a href="https://github.com/jslegenddev" target="_blank">Github</a>!`,
-    "cs-degree": `This is my CS degree. I hung it on the wall because I'm proud of it. It was a very theoretical degree but I think it gave me a good foundation.`,
-    "sofa-table": `That's my sofa. I like to relax here and watch YouTube. 
-    I also make game programming tutorials on YouTube. Go sub to <a href="https://youtube.com/@jslegenddev" target="_blank">my channel</a>! (If you like the content)
-    You'll learn how I built this portfolio you're currently playing through!`,
-    tv: `That's my TV. I've been watching tech youtubers a lot recently like :
-        <a href="https://www.youtube.com/@ThePrimeTimeagen" target="_blank">Theprimeagen</a>, <a href="https://www.youtube.com/@t3dotgg" target="_blank">Theo - t3.gg</a>,
-    <a href="https://www.youtube.com/@PirateSoftware" target="_blank">PirateSoftware</a> (sometimes) and <a href="https://www.youtube.com/@MelkeyDev" target="_blank">Melkey</a>!`,
-    bed: `This where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
-    resume: `This is my desk and on it is my resume. <a href="https://github.com/Almesaody/Resume/blob/main/JSLegend%20Resume-1.pdf" target="_blank">Check it out?</a>
-    Contact me at jslegend@protonmail.com if you have any interesting job opportunities!`,
-    projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-    Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Learn more about how to use
-    Kaboom.js by watching some of my tutorials <a href="https://youtube.com/@Almesaody" target="_blank">here</a>.`,
-    library: `There are a lot of programming books on my shelves. There is even one in French (I also speak French btw).
-    I probably only read one of them. Who else compulsively buys technical books without ever finishing them?`,
-    exit: `If you want to exit Almesaody's portfolio, just close the tab.`,
+  pc: `This is my PC. I work mostly in JavaScript/TypeScript these days.
+    I've made a couple of games in that language. I like Golang and Python as well. Anyway regardless of the language, I just like programming.`,
+  "cs-degree": `This is my CS degree. I hanged it on the wall because I'm proud to have it. It was a very theoretical degree but I think it gave me a good foundation.`,
+  "sofa-table": `That's my sofa and my table. I like to relax here and sometimes play games or watch YouTube.`,
+  tv: `That's my TV. I have a ps4 hooked up here where I play Elden Ring.`,
 };
--- src/utils.js
+++ src/utils.js
@@ -3,13 +3,10 @@ export function displayDialogue(text, onDisplayEnd) {
     const dialogue = document.getElementById("dialogue");
 
     dialogueUI.style.display = "block";
-
     let index = 0;
-    let currentText = "";
     const intervalRef = setInterval(() => {
         if(index < text.length) {
-            currentText += text[index];
-            dialogue.innerHTML = currentText;
+            dialogue.innerHTML += text[index];
             index++;
             return;
         }
@@ -22,20 +19,10 @@ export function displayDialogue(text, onDisplayEnd) {
     function onCloseBtnClick() {
         onDisplayEnd();
         dialogueUI.style.display = "none";
-        display.innerHTML = "";
+        dialogue.innerHTML = "";
         clearInterval(intervalRef);
         closeBtn.removeEventListener("click", onCloseBtnClick);
     }
 
     closeBtn.addEventListener("click", onCloseBtnClick);
-}
-
-export function setCamScale(k) {
-    const resizeFactor = k.width() / k.height();
-    if(resizeFactor < 1) {
-        k.camScale(k.vec2(1));
-        return;
-    }
-
-    k.camScale(k.vec2(1.5));
 }
