e33a232708d7d08467d37d19be545f47bdcf56cd
Almesaody <120131207+Almesaody@users.noreply.github.com>
2024-05-04 05:11:18 +0300

    trying second fix

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig ({
-    base: "https://github.com/Almesaody/2DGame/",
+    base: "/dist/",
     build: {
         minify: "terser",
     },
